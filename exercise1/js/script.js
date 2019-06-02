8f7f7e022b37f3590db941bd7ca69ad3d319ed2d
Saul Lopez Molina <saulsjlmclash@gmail.com>
2019-06-02 21:03:55 +0000

    i have added first exercise

---
+++ exercise1/js/script.js
@@ -0,0 +1,21 @@
+(function () {
+  const btnMenu = document.querySelector('.js-btn-menu');
+  const menu = document.querySelector('.js-menu');
+
+  function createMenu() {
+    const ico = document.createElement('img');
+    ico.setAttribute('src', 'img/menu.png');
+    btnMenu.appendChild(ico);
+  }
+
+  function init() {
+    createMenu();
+  }
+
+  window.addEventListener("load", function(event) {
+    init();
+  });
+  btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-hidden');
+  });
+}());
